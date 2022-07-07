093b9ec30c9e8f1b0a284e6cef847a4dac8f1052
Jules MUNTZ-BERGER <julmuntz@e1r11p16.42.fr>
2022-07-07 15:07:42 +0200

    Variadic Function Prototype

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,30 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 13:59:36 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/07/06 21:03:41 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/07/07 15:05:28 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-// int	ft_printf(const char *format, ...)
+
 int	ft_printf(const char *format, ...)
 {
-	va_list ap;
-	char *str;
+	va_list	ap;
 	va_start(ap, format);
+	char	*str;
 
-	str = va_arg(ap, char*);
-
-	printf("%s", str);
-	return(0);
+	str = va_arg(ap, char *);
+	while (str)
+	{
+		ft_putstr(str);
+		str = va_arg(ap, char *);
+	}
+	va_end(ap);
+	return (0);
 }
 
-// int main(int ac, char **av)
-// {
-// 	ft_printf();
-// 	return(0);
-// }
+int	main(int ac, char **av)
+{
+	ft_printf("Atrsdthbtr\n", "Bikyfutd6rye\n", "Ckhjgyfhtdg\n", "Djhcgnxfds\n", "Erhsjgafv8ps\n");
+	return (0);
+}
